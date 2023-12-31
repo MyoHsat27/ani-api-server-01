567293a0b07e933bdd001eff95139e304dd59c48
Myo Hsat <hsatmyo@gmail.com>
2023-12-31 13:30:25 +0630

    add[myo-hsat]: MangaType model, factory, migration

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,10 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use App\Models\MangaType;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +16,23 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        // Test User Account
+        User::factory()->create([
+            'name'              => 'Tester',
+            'slug'              => 'tester',
+            'email'             => 'test@gmail.com',
+            'email_verified_at' => now(),
+            'password'          => Hash::make('yngWIE500'),
+            'remember_token'    => Str::random(10),
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        // Default Manga Types
+        $mangaTypes = ['manga', "manhwa", "manhua"];
+        foreach ($mangaTypes as $type) {
+            MangaType::factory()->create([
+                'type' => $type,
+                'slug' => Str::slug($type),
+            ]);
+        }
     }
 }
