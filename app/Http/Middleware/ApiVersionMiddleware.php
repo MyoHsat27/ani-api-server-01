429da53f74d504a666807e37cc6490d4cdf2613a
Myo Hsat <hsatmyo@gmail.com>
2024-01-01 18:06:12 +0630

    add[myo-hsat]: Api Version Middleware

---
+++ app/Http/Middleware/ApiVersionMiddleware.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class ApiVersionMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  Closure(Request): (Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $requestedVersion = $request->segment(2);
+        $supportedVersion = ['v1', 'v2'];
+        if (!in_array($requestedVersion, $supportedVersion)) {
+            return response()->json(['error' => 'Unsupported API Version']);
+        }
+        $request->attributes->add(['api_version' => $requestedVersion]);
+
+        return $next($request);
+    }
+}
